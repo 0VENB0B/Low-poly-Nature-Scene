7ccdd70b9e4c0447e6fca502b464ad6dfb5a4b04
BOB <peecoc789987@gmail.com>
2025-04-20 03:43:54 +0700

    FirstCommit

---
+++ Assets/Scripts/FOVChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class FOVChanger : MonoBehaviour
+{
+    [SerializeField] Volume volume; // Assign in Inspector
+    private DepthOfField dof;
+    [SerializeField] FirstPersonController firstPersonController;
+    public float targetStart;
+    public float targetEnd;
+
+    private void Update() {
+        if (firstPersonController.isZoomed){
+            if (volume.profile.TryGet(out dof)){
+                dof.gaussianStart.value = Mathf.Lerp(dof.gaussianStart.value, targetStart, firstPersonController.zoomStepTime * Time.deltaTime);
+                dof.gaussianEnd.value = Mathf.Lerp(dof.gaussianStart.value, targetStart, firstPersonController.zoomStepTime * Time.deltaTime);
+            }
+            
+        }else{
+            if (volume.profile.TryGet(out dof)){
+                dof.gaussianStart.value = Mathf.Lerp(dof.gaussianStart.value, 100f, firstPersonController.zoomStepTime * Time.deltaTime);;
+                dof.gaussianEnd.value = Mathf.Lerp(dof.gaussianStart.value, 100f, firstPersonController.zoomStepTime * Time.deltaTime);;
+            }
+        }
+    }
+}
